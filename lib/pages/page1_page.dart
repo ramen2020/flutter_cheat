8e94b2b25e759fd4889c3e4fc7df88964a887883
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-05 08:18:06 +0900

    画面遷移する

--- lib/pages/page1_page.dart
+++ lib/pages/page1_page.dart
@@ -10,8 +10,28 @@ class Page1 extends StatelessWidget {
         child: ElevatedButton(
           child: Text('placetice1'),
           onPressed: () {
-            //
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Page6(),
+                ));
           },
+        ),
+      ),
+    );
+  }
+}
+
+class Page6 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      alignment: Alignment.center,
+      child: Center(
+        child: ElevatedButton(
+          child: Text('遷移しました'),
+          onPressed: () {},
         ),
       ),
     );
