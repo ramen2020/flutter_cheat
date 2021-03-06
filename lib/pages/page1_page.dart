77834813afdb7d2aa57c3da7af05bfab8f2fff08
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-06 23:45:02 +0900

    colum使うように修正

--- lib/pages/page1_page.dart
+++ lib/pages/page1_page.dart
@@ -6,18 +6,21 @@ class Page1 extends StatelessWidget {
     return Container(
       color: Colors.red,
       alignment: Alignment.center,
-      child: Center(
-        child: ElevatedButton(
-          child: Text('placetice1'),
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Page6(),
-                ));
-          },
-        ),
-      ),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('placetice1'),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Page6(),
+                    ));
+              },
+            ),
+          ]),
     );
   }
 }
@@ -33,25 +36,25 @@ class Page6 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
-                child: Text(
-                  '遷移しました',
-                  style: TextStyle(
-                    color: Colors.yellow,
-                    fontSize: 30,
-                  ),
+              child: Text(
+                '遷移しました',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  fontSize: 30,
                 ),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black, //ボタンの背景色
-                ),
-                onPressed: () {},
               ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black, //ボタンの背景色
+              ),
+              onPressed: () {},
+            ),
             ElevatedButton(
-                child: Text('戻る'),
-                onPressed: () {
-                  // 1つ前に戻る
-                  Navigator.pop(context);
-                },
-              ),
+              child: Text('戻る'),
+              onPressed: () {
+                // 1つ前に戻る
+                Navigator.pop(context);
+              },
+            ),
             ElevatedButton(
               onPressed: null,
               child: Text(
