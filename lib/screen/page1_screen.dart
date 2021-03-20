ef125309bcd28973b8921ed07eee6edbcfe49f77
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-20 23:25:05 +0900

    page6_screenをファイル分ける

--- lib/screen/page1_screen.dart
+++ lib/screen/page1_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import './page6_screen.dart';
 
 class Page1 extends StatelessWidget {
   @override
@@ -265,45 +266,4 @@ Widget indicatorButton(BuildContext context) {
       Navigator.of(context).pop();
     },
   );
-}
-
-class Page6 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      alignment: Alignment.center,
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: Text(
-                '遷移しました',
-                style: TextStyle(
-                  color: Colors.yellow,
-                  fontSize: 30,
-                ),
-              ),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.black, //ボタンの背景色
-              ),
-              onPressed: () {},
-            ),
-            ElevatedButton(
-              child: Text('戻る'),
-              onPressed: () {
-                // 1つ前に戻る
-                Navigator.pop(context);
-              },
-            ),
-            ElevatedButton(
-              onPressed: null,
-              child: Text(
-                "これは押せないボタン",
-              ),
-            ),
-          ]),
-    );
-  }
 }
