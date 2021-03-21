2cd6ac417e2819c5ecbd7fde99eead5624b43b6a
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-21 22:10:01 +0900

    buttonを置く画面へ遷移するように修正

---
+++ lib/screen/button/buttons_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ButtonsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue[50],
+      child: Text('ButtonsScreen'),
+      alignment: Alignment.center,
+    );
+  }
+}
--- lib/screen/page2_screen.dart
+++ lib/screen/page2_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import './button/buttons_screen.dart';
 
 class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.yellow,
-      child: Text('page2'),
-      alignment: Alignment.center,
-    );
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // 画面遷移
+              ElevatedButton(
+                child: Text('buttons_screen'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ButtonsScreen(),
+                      ));
+                },
+              ),
+            ]));
   }
 }
