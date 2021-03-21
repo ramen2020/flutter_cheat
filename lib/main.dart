35c21d82e49e11787c541316cec0b179156d6de7
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-21 22:34:01 +0900

    add FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -33,56 +33,53 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _currentIndex = 0;
-  final List<Widget> _children = [
-    Page1(),
-    Page2(),
-    Page3(),
-    Page4(),
-    Page5()
-  ];
+  final List<Widget> _children = [Page1(), Page2(), Page3(), Page4(), Page5()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+            canvasColor: Colors.blue,
+            primaryColor: Colors.red,
+            textTheme: Theme.of(context)
+                .textTheme
+                .copyWith(caption: TextStyle(color: Colors.white))),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Page1',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_call),
+              label: 'Page2',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Page3'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.offline_bolt),
+              label: 'Page4',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.edit_attributes),
+              label: 'Page5',
+            )
+          ],
         ),
-        body: _children[_currentIndex],
-        bottomNavigationBar: Theme(
-          data: Theme.of(context).copyWith(
-              canvasColor: Colors.blue,
-              primaryColor: Colors.red,
-              textTheme: Theme.of(context)
-                  .textTheme
-                  .copyWith(caption: TextStyle(color: Colors.white))),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            onTap: onTabTapped,
-            currentIndex: _currentIndex,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Page1',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add_call),
-                label: 'Page2',
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person),
-                  label: 'Page3'
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.offline_bolt),
-                label: 'Page4',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.edit_attributes),
-                label: 'Page5',
-              )
-            ],
-          ),
-        ));
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.navigation),
+        backgroundColor: Colors.green,
+      ),
+    );
   }
 
   void onTabTapped(int index) {
