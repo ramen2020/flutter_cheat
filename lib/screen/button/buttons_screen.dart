8235f5719162b45f3bd563e4c09ae07a110a6ce4
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-24 20:10:24 +0900

    add button_screen navigation bar

--- lib/screen/button/buttons_screen.dart
+++ lib/screen/button/buttons_screen.dart
@@ -3,14 +3,18 @@ import 'package:flutter/material.dart';
 class ButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        color: Colors.red[100],
-        alignment: Alignment.center,
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextButton(onPressed: () {}, child: Text('TextButton')),
-            ]));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ButtonsScreen'),
+        ),
+        body: Container(
+            color: Colors.red[100],
+            alignment: Alignment.center,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextButton(onPressed: () {}, child: Text('TextButton')),
+                ])));
   }
 }
