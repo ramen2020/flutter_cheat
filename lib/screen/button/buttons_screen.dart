95d811b21b3aaf487edc8e86be8b8db43d4a30fd
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-25 22:12:37 +0900

    add button floatingAction

--- lib/screen/button/buttons_screen.dart
+++ lib/screen/button/buttons_screen.dart
@@ -4,17 +4,23 @@ class ButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('ButtonsScreen'),
-        ),
-        body: Container(
-            color: Colors.red[100],
-            alignment: Alignment.center,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextButton(onPressed: () {}, child: Text('TextButton')),
-                ])));
+      appBar: AppBar(
+        title: Text('ButtonsScreen'),
+      ),
+      body: Container(
+          color: Colors.red[100],
+          alignment: Alignment.center,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextButton(onPressed: () {}, child: Text('TextButton')),
+              ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.person),
+        backgroundColor: Colors.pink[200],
+      ),
+    );
   }
 }
