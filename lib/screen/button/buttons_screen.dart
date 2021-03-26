85d7151647c1e75381a9f39ed2f140064d492c54
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-03-26 23:01:11 +0900

    add PopupMenuItem

--- lib/screen/button/buttons_screen.dart
+++ lib/screen/button/buttons_screen.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import '../page6_screen.dart';
 
-class ButtonsScreen extends StatelessWidget {
+class ButtonsScreen extends StatefulWidget {
+  @override
+  _ButtonsScreenState createState() => _ButtonsScreenState();
+}
+
+class _ButtonsScreenState extends State<ButtonsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ButtonsScreen'),
+        actions: <Widget>[
+          Theme(
+            data: Theme.of(context).copyWith(
+                textTheme: TextTheme().apply(bodyColor: Colors.blue),
+                dividerColor: Colors.white,
+                iconTheme: IconThemeData(color: Colors.white)),
+            child: PopupMenuButton<int>(
+              color: Colors.blue,
+              itemBuilder: (context) => [
+                PopupMenuItem<int>(value: 0, child: Text("page6")),
+                PopupMenuItem<int>(value: 1, child: Text("page6")),
+                PopupMenuDivider(),
+                PopupMenuItem<int>(
+                    value: 2,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.logout,
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          width: 7,
+                        ),
+                        Text("ログアウト")
+                      ],
+                    )),
+              ],
+              onSelected: (item) => selectedItem(context, item),
+            ),
+          ),
+        ],
       ),
       body: Container(
           color: Colors.red[100],
@@ -22,5 +59,22 @@ class ButtonsScreen extends StatelessWidget {
         backgroundColor: Colors.pink[200],
       ),
     );
+  }
+
+  void selectedItem(BuildContext context, item) {
+    switch (item) {
+      case 0:
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => Page6()));
+        break;
+      case 1:
+        print("Privacy Clicked");
+        break;
+      case 2:
+        print("User Logged out");
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => Page6()), (route) => false);
+        break;
+    }
   }
 }
