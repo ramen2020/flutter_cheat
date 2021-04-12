742be1a6e832058b9ff0a07e96f05ec1d8be7c12
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-04-12 21:01:31 +0900

    sliderを実装

--- lib/screen/slider/slider_view_screen.dart
+++ lib/screen/slider/slider_view_screen.dart
@@ -6,6 +6,8 @@ class SliderViewScreen extends StatefulWidget {
 }
 
 class _SliderViewScreenState extends State<SliderViewScreen> {
+  double _currentSliderValue = 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +21,19 @@ class _SliderViewScreenState extends State<SliderViewScreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text("SliderViewScreen")
-                  ])));
+                  Text("SliderViewScreen"),
+                  Slider(
+                    value: _currentSliderValue,
+                    min: 0,
+                    max: 100,
+                    divisions: 5,
+                    label: _currentSliderValue.round().toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderValue = value;
+                      });
+                    },
+                  ),
+                ])));
   }
 }
