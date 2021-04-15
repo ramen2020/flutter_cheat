b9299addabd61984294faba6aa2eeb09105f1f19
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-04-16 00:02:42 +0900

    sliderをtextとsliderでひとまとめにする。

--- lib/screen/slider/slider_view_screen.dart
+++ lib/screen/slider/slider_view_screen.dart
@@ -22,19 +22,22 @@ class _SliderViewScreenState extends State<SliderViewScreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text("SliderViewScreen"),
-                  Slider(
-                    value: _currentSliderValue,
-                    min: 0,
-                    max: 100,
-                    divisions: 5,
-                    label: _currentSliderValue.round().toString(),
-                    onChanged: (double value) {
-                      setState(() {
-                        _currentSliderValue = value;
-                      });
-                    },
-                  ),
+                  Column(children: <Widget>[
+                    Text("Slider"),
+                    Slider(
+                      value: _currentSliderValue,
+                      min: 0,
+                      max: 100,
+                      divisions: 5,
+                      label: _currentSliderValue.round().toString(),
+                      onChanged: (double value) {
+                        setState(() {
+                          _currentSliderValue = value;
+                        });
+                      },
+                    ),
+                  ]),
+                  Text("Custom Slider"),
                   SliderTheme(
                     data: SliderTheme.of(context).copyWith(
                       trackHeight: 10,
