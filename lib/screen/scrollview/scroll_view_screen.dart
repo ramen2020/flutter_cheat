1909909c47659dcd5710d5da73518e40009c3542
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-04-20 00:06:24 +0900

    タイトルとwidgetをまとめるように修正

--- lib/screen/scrollview/scroll_view_screen.dart
+++ lib/screen/scrollview/scroll_view_screen.dart
@@ -14,58 +14,54 @@ class ScrollViewScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  scrollViewRightFixed(context),
-                  SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(children: <Widget>[
-                        SizedBox(
-                            width: 100, height: 50, child: const Text('A')),
-                        SizedBox(
-                            width: 100, height: 50, child: const Text('B')),
-                        SizedBox(
-                            width: 100, height: 50, child: const Text('C')),
-                        SizedBox(
-                            width: 100, height: 50, child: const Text('D')),
-                        SizedBox(
-                            width: 100, height: 50, child: const Text('E')),
-                        SizedBox(
-                            width: 100, height: 50, child: const Text('F')),
-                        SizedBox(
-                            width: 100, height: 50, child: const Text('G')),
-                      ]))
+                  Column(children: <Widget>[
+                    Text("左は固定で、右はスクロール"),
+                    scrollViewRightFixed(context),
+                  ]),
+                  Column(children: <Widget>[
+                    Text("スクロール　only"),
+                    SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(children: <Widget>[
+                          SizedBox(
+                              width: 100, height: 50, child: const Text('A')),
+                          SizedBox(
+                              width: 100, height: 50, child: const Text('B')),
+                          SizedBox(
+                              width: 100, height: 50, child: const Text('C')),
+                          SizedBox(
+                              width: 100, height: 50, child: const Text('D')),
+                          SizedBox(
+                              width: 100, height: 50, child: const Text('E')),
+                          SizedBox(
+                              width: 100, height: 50, child: const Text('F')),
+                          SizedBox(
+                              width: 100, height: 50, child: const Text('G')),
+                        ]))
+                  ]),
                 ])));
   }
 }
 
 // 左を固定で、右は横スクロール
 Widget scrollViewRightFixed(BuildContext context) {
-  return Column(children: [
-    Text(
-      '左を固定で、右は横スクロール',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 20,
-        color: Theme.of(context).primaryColor,
-      ),
-    ),
-    Container(
-        width: 1030,
-        child: Row(children: <Widget>[
-          SizedBox(
-            width: 100,
-            height: 100,
-            child: Text('固定'),
-          ),
-          Expanded(
-              child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(children: <Widget>[
-                    SizedBox(width: 100, height: 50, child: const Text('A')),
-                    SizedBox(width: 100, height: 50, child: const Text('B')),
-                    SizedBox(width: 100, height: 50, child: const Text('C')),
-                    SizedBox(width: 100, height: 50, child: const Text('D')),
-                    SizedBox(width: 100, height: 50, child: const Text('E')),
-                  ])))
-        ]))
-  ]);
+  return Container(
+      width: 1030,
+      child: Row(children: <Widget>[
+        SizedBox(
+          width: 100,
+          height: 100,
+          child: Text('固定'),
+        ),
+        Expanded(
+            child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: <Widget>[
+                  SizedBox(width: 100, height: 50, child: const Text('A')),
+                  SizedBox(width: 100, height: 50, child: const Text('B')),
+                  SizedBox(width: 100, height: 50, child: const Text('C')),
+                  SizedBox(width: 100, height: 50, child: const Text('D')),
+                  SizedBox(width: 100, height: 50, child: const Text('E')),
+                ])))
+      ]));
 }
