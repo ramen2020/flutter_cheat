65c207048aecab877808d173a6147a03f8ca8b5a
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-04-23 23:01:02 +0900

    progress screenの大枠を完成

---
+++ lib/screen/progress/progress_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ProgressScreen extends StatefulWidget {
+  @override
+  _ProgressScreenState createState() => _ProgressScreenState();
+}
+
+class _ProgressScreenState extends State<ProgressScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('SliderViewScreen'),
+        ),
+        body: Container(
+            color: Colors.yellow[100],
+            alignment: Alignment.center,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(children: <Widget>[
+                    
+                  ]),
+                  Column(children: <Widget>[
+                    
+                  ]
+                  )
+                ])));
+  }
+}
