894c7a26e812230ed053cf379d1fd2b5b62b8a46
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-05-20 21:23:34 +0900

    add card_screenファイル追加

---
+++ lib/screen/card/card_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Card extends StatelessWidget {
+  @override
+ Widget build(BuildContext context) {
+    return Container(
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            ]));
+  }
+}
