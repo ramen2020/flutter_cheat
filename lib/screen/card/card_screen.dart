7eba24232ce76364d39e04299517395afc05b321
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-05-20 21:39:27 +0900

    add card_screen 大枠追加

--- lib/screen/card/card_screen.dart
+++ lib/screen/card/card_screen.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 
 class Card extends StatelessWidget {
   @override
- Widget build(BuildContext context) {
-    return Container(
-        color: Colors.red,
-        alignment: Alignment.center,
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-            ]));
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('ButtonsScreen')),
+        body: Container(
+            color: Colors.red[100],
+            alignment: Alignment.center,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      color: Colors.red,
+                      alignment: Alignment.center,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            
+                          ]))
+                ])));
   }
 }
