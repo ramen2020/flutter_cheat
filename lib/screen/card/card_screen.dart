aeb0b99f94c4aed439e93f6ed52bcfc28edd8ccb
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-05-24 20:15:33 +0900

    add card key

--- lib/screen/card/card_screen.dart
+++ lib/screen/card/card_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
+  const CardScreen({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
