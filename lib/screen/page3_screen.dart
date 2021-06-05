a7443f41f0f7813984e6321b51219f839dbe77d7
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-06 07:37:56 +0900

    Delete unused code.

--- lib/screen/page3_screen.dart
+++ lib/screen/page3_screen.dart
@@ -12,15 +12,15 @@ class Page3 extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               // 画面遷移
-              ElevatedButton(
-                  child: Text('ScrollViewScreen'),
-                  onPressed: () {
-                    Navigator.push<void>(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ScrollViewScreen(),
-                        ));
-                  }),
+              // ElevatedButton(
+              //     child: Text('ScrollViewScreen'),
+              //     onPressed: () {
+              //       Navigator.push<void>(
+              //           context,
+              //           MaterialPageRoute(
+              //             builder: (context) => ScrollViewScreen(),
+              //           ));
+              //     }),
 
               // ElevatedButton(
               //   child: Text('checkbox_screen'),
