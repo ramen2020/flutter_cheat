9e4f140b96b3e3e9a0605203ee643ddf9cc60503
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-06-23 00:00:05 +0900

    transition sample page1

--- lib/screen/page3_screen.dart
+++ lib/screen/page3_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './webview/webview_screen.dart';
+import './sample1/sample1_screen.dart';
 
 class Page3 extends StatelessWidget {
   @override
@@ -19,6 +20,16 @@ class Page3 extends StatelessWidget {
                         context,
                         MaterialPageRoute(
                           builder: (context) => WebViewScreen(),
+                        ));
+                  }),
+
+              ElevatedButton(
+                  child: Text('Sample1'),
+                  onPressed: () {
+                    Navigator.push<void>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Sample1Screen(),
                         ));
                   }),
 
