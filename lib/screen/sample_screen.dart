a078bf8100e3aacd41683d60e114856eab8eb1e4
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-01 12:46:16 +0900

    add lib/screen/smaple_screen.dart

---
+++ lib/screen/sample_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'webview/webview_screen.dart';
+import 'sample/sample1_screen.dart';
+import 'sample/sample2_screen.dart';
+
+class SampleScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // 画面遷移
+              ElevatedButton(
+                  child: Text('WebViewScreen'),
+                  onPressed: () {
+                    Navigator.push<void>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => WebViewScreen(),
+                        ));
+                  }),
+
+              ElevatedButton(
+                  child: Text('Sample1'),
+                  onPressed: () {
+                    Navigator.push<void>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Sample1Screen(),
+                        ));
+                  }),
+
+              ElevatedButton(
+                child: Text('Sample2'),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Sample2Screen(),
+                      ));
+                },
+              )
+            ]));
+  }
+}
