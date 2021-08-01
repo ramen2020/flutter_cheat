64f845fd8eb3e899af305a5f607f2009af4b7762
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-01 12:20:01 +0900

    add sample2 frame

--- lib/screen/sample1/sample2_screen.dart
+++ lib/screen/sample1/sample2_screen.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
 
+class Sample2Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: CustomScrollView(slivers: <Widget>[
+      SliverAppBar(
+          brightness: Brightness.dark,
+          expandedHeight: 240,
+          flexibleSpace: Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset('assets/ramen/ramen1.jpeg', fit: BoxFit.cover),
+              Positioned(
+                bottom: 15,
+                left: 20,
+                child: Text(
+                  'Ramen sample',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                ),
+              ),
+            ],
+          )),
+      SliverList(
+          delegate: SliverChildListDelegate([
+        SingleChildScrollView(
+            child: Container(
+                color: Colors.grey[200],
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[],
+                  ),
+                ))),
+      ]))
+    ]));
+  }
+}
