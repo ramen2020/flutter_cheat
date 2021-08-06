d7be149c23a2468666a38dfae0d17cf95816b812
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-08-06 21:02:04 +0900

    add RefreshIndicator

---
+++ lib/screen/refreshIndicator/refreshIndicator_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class RefreshIndicatorScreen extends StatefulWidget {
+  @override
+  _RefreshIndicatorScreenState createState() => _RefreshIndicatorScreenState();
+}
+
+class _RefreshIndicatorScreenState extends State<RefreshIndicatorScreen> {
+  List _titleList = ["test"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('RefreshIndicatorScreen'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await _addText();
+        },
+        child: ListView(
+          children: _titleList
+              .map((title) => ListTile(
+                    title: Text(title),
+                  ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+
+  _addText() {
+    setState(() {
+      _titleList.add("test");
+    });
+  }
+}
